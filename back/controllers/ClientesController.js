5e093879ba9d0aa14cc5c43344f5debf208c1b1b
JOSE SOSA <josea_1998@hotmail.com>
2023-01-14 19:38:27 -0500

    Cambio de nombre de los contradores, modelos y rutas de singular a plural

---
+++ back/controllers/ClientesController.js
@@ -0,0 +1,101 @@
+import { Clientes } from "../models/ClientesModel.js";
+import { Sequelize } from "sequelize";
+
+const crearCliente = async (req, res) => {
+    try {
+        await Clientes.create(req.body);
+        res.json({
+            message: "Cliente creado correctamente",
+            estado: true
+        })
+
+    } catch (error) {
+        res.json({
+            message: "El cliente no se pudo crear " + error,
+            estado: false
+        });
+    }
+
+}
+
+const mostrarClientes = async (req, res) => {
+    try {
+        const clientes = await Clientes.findAll();
+        res.json(clientes)
+
+    } catch (error) {
+        res.json({
+            message: "El cliente no se pudo crear " + error
+        });
+
+    }
+
+}
+
+
+const mostrarCliente = async (req, res) => {
+    try {
+        const { Op }= Sequelize
+        const cliente = await Clientes.findOne({
+            where: {
+                [Op.or]: [
+                {idCliente: req.params.id},
+                {cedCliente: req.params.id},
+                ]
+            }
+        });
+        res.json(cliente)
+
+    } catch (error) {
+        res.json({
+            message: "El cliente no se puede mostrar " + error
+        });
+    }
+}
+
+const editarCliente = async (req, res) => {
+
+    try {
+        const { Op }= Sequelize
+        await Clientes.update(req.body, {
+            where: {
+                [Op.or]: [
+                {idCliente: req.params.id},
+                {cedCliente: req.params.id},
+                ]
+            }
+        });
+        res.json({
+            message: "El cliente se ha actualizado correctamente "
+        });
+
+    } catch (error) {
+        res.json({
+            message: "El cliente no se puede editar " + error
+        });
+    }
+
+
+
+}
+
+const borrarCliente = async (req, res) => {
+    try {
+
+        await Clientes.destroy({
+            where: { idCliente: req.params.id }
+        });
+        res.json({
+            message: "El cliente se borrado correctamente "
+        });
+
+    } catch (error) {
+        res.json({
+            message: "El cliente no se puede borrar " + error
+        });
+
+    }
+
+}
+
+export { crearCliente, mostrarCliente, mostrarClientes, editarCliente, borrarCliente }
