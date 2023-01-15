ab5d610c5f986cdf107508eef89ef24075ddc565
JOSE SOSA <josea_1998@hotmail.com>
2023-01-15 15:31:04 -0500

    Se ingresan datos a los paises, departamentos y ciudades. Se crea una carpeta con los json.

--- back/models/CiudadesModel.js
+++ back/models/CiudadesModel.js
@@ -1,5 +1,17 @@
 import dataBase from "../database/dataBase.js";
 import { DataTypes } from "sequelize";
+import fs from 'fs';
+import path from "path"
+import  join  from 'path';
+
+
+
+/* const fs = fs;
+const path = path */
+
+/* const ciudadesPath = path.join(__dirname, '../json/ciudades.json');
+const ciudadesJSON = JSON.parse(fs.readFileSync(ciudadesPath, 'utf-8')); */
+
 
 const Ciudades = dataBase.define('ciudades', {
     id: {
@@ -28,6 +40,19 @@ const Ciudades = dataBase.define('ciudades', {
         allowNull: false
     }
 });
+
+
+
+/* ciudades.forEach(ciudad => Ciudades.findOrCreate(
+    {
+        where: { id: ciudad.id }, defaults: ciudad
+    }
+));  */
+Ciudades.bulkCreate(ciudadesJSON, { ignoreDuplicates: true }).then(() => {
+    console.log('Datos insertados correctamente');
+}).catch((error) => {
+    console.log(error);
+})
 
 Ciudades.sync({ force: false });
 
