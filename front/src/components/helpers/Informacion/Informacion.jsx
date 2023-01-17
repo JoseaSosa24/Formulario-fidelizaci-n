dc74852cf3ee11398188b8c47224604b6d339ccb
JOSE SOSA <josea_1998@hotmail.com>
2023-01-17 03:27:39 -0500

    Se crea componente informacion

---
+++ front/src/components/helpers/Informacion/Informacion.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Galeria } from '../Galeria/Galeria'
+
+export const Informacion = () => {
+    return (
+        <section className='informacion'>
+        <Galeria />
+        </section>
+    )
+}
