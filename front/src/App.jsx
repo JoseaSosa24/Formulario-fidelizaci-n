6d155672c858209ef553287af771e5be95feb1ea
JOSE SOSA <josea_1998@hotmail.com>
2023-01-17 03:28:16 -0500

    Se insetan componentes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import { FormularioClientes } from './components/helpers/Formulario/Clientes/FormularioClientes'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Main } from './components/layouts/Main'
+import { Footer } from './components/helpers/Footer/Foter'
+import { Header } from './components/helpers/Header/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <FormularioClientes />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
