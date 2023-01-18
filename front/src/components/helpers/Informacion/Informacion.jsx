fedd3cb4378b6f8c5dc62e3283969620bc251499
JOSE SOSA <josea_1998@hotmail.com>
2023-01-18 15:27:26 -0500

    Se agregan informacion al componente para renderizarla

--- front/src/components/helpers/Informacion/Informacion.jsx
+++ front/src/components/helpers/Informacion/Informacion.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Galeria } from '../Galeria/Carrusel'
 
 export const Informacion = () => {
+    const [contentVisible, setcontentVisible] = useState(false);
+    useEffect(() => {
+        setcontentVisible(true)
+    })
+
     return (
-        <section className='informacion'>
-        <Galeria />
+        <section className="informacion">
+            <section className={contentVisible ? "content-visible titulos-informacion text-center mb-2 container" : 'content-hidden'}>
+                <h1 className='numero-descuento'>40%</h1>
+                <h6 className='descuento'>de descuento en TODAS</h6>
+                <h3 className=''>nuestras Marcas</h3>
+                <Galeria />
+            </section>
+
         </section>
     )
 }
