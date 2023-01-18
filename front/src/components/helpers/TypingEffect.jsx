489b2dbe981f48d56c51c3bebef43068f9d06e6a
JOSE SOSA <josea_1998@hotmail.com>
2023-01-18 00:15:23 -0500

    Se crea componente TypingEffect

--- front/src/components/helpers/TypingEffect.jsx
+++ front/src/components/helpers/TypingEffect.jsx
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Typewriter from "typewriter-effect";
-import '../helpers/Galeria/galeria.css'
+import '../helpers/Galeria/carrusel.css'
 
 const TypingEffect = ({ className, text }) => {
     return (
-        <>
-            {text.map((t, index) => (
-                <Typewriter
-                    key={index}
-                    className={`text-${index}`}
-                    options={{
-                        cursor: "",
-                        
-                        strings: [t],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                />
-            ))}
-        </>
+        <Typewriter
+            options={{
+                strings: [text],
+                autoStart: true,
+                loop: true,
+            }}
+        />
     );
 };
 
