2974f6c58f12de202d7e08560a4ce283e53789dd
JOSE SOSA <josea_1998@hotmail.com>
2023-01-17 22:16:49 -0500

    Se agrega columna de contacto en el footer.

--- front/src/components/helpers/Footer/Foter.jsx
+++ front/src/components/helpers/Footer/Foter.jsx
@@ -6,25 +6,52 @@ export const Footer = () => {
             <footer>
                 <div class="container mt-4">
 
-                    <div class="row">
-                        <div class="col-md-6 item text">
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-md-4 item text">
                             <h3>Grupo Uribe</h3>
                             <p>Conjunto de empresas ubicada en Medellín del sector textil con representación,
                                 y/o producción y comercialización para Colombia de las marcas Chevignon,
                                 Naf Naf, Americanino, Espit, Rifle, American Eagle, MNG.</p>
                         </div>
-                        <div class="col-sm-6 col-md-3 item text d-flex flex-column align-items-center">
+                        <div class="col-sm-6 col-md-3 item text">
                             <h3>Tamaño de la empresa</h3>
                             <p>1001-5000 empleados</p>
                         </div>
-                        <div class="col-sm-6 col-md-3 item text d-flex flex-column align-items-center ">
+                        <div class="col-sm-6 col-md-2 item text ">
                             <h3>Fundación</h3>
                             <p>1907</p>
                         </div>
+                        <div class="col-sm-6 col-md-3 item text  ">
+                            <section>
+                                <h3>Contacto</h3>
+                                <p>
+                                    MEDELLIN, ANTIOQUIA
+                                </p>
+                            </section>
+                            <section>
+
+                                <p>Carrera 52 # 19 - 80 avenida guayabal</p>
+                            </section>
+                            <section>
+                                <p>050024 Medellin</p>
+                            </section>
+                            <section>
+                                <p>Colombia</p>
+                            </section>
+                            <section>
+                                <p>+57 (604) 6071213</p>
+                            </section>
+                            <section>
+                                <p>
+                                    <a href="#">mercadeo@johnuribe.com.co</a>
+                                </p>
+                            </section>
+                        </div>
+
                     </div>
                     <p class="copyright">Grupo Uribe © 2023</p>
                 </div>
-            </footer>
-        </div>
+            </footer >
+        </div >
     )
 }
