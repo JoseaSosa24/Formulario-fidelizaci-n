5aa66f3d7858f462882bf2e1e82712f68b21554f
JOSE SOSA <josea_1998@hotmail.com>
2023-01-17 20:26:30 -0500

    se le agrega información al footer.

--- front/src/components/helpers/Footer/Foter.jsx
+++ front/src/components/helpers/Footer/Foter.jsx
@@ -4,31 +4,25 @@ export const Footer = () => {
     return (
         <div class="footer-dark">
             <footer>
-                <div class="container">
+                <div class="container mt-4">
+
                     <div class="row">
-                        <div class="col-sm-6 col-md-3 item">
-                            <h3>Services</h3>
-                            <ul>
-                                <li><a href="#">Web design</a></li>
-                                <li><a href="#">Development</a></li>
-                                <li><a href="#">Hosting</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-sm-6 col-md-3 item">
-                            <h3>About</h3>
-                            <ul>
-                                <li><a href="#">Company</a></li>
-                                <li><a href="#">Team</a></li>
-                                <li><a href="#">Careers</a></li>
-                            </ul>
-                        </div>
                         <div class="col-md-6 item text">
-                            <h3>Company Name</h3>
-                            <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                            <h3>Grupo Uribe</h3>
+                            <p>Conjunto de empresas ubicada en Medellín del sector textil con representación,
+                                y/o producción y comercialización para Colombia de las marcas Chevignon,
+                                Naf Naf, Americanino, Espit, Rifle, American Eagle, MNG.</p>
                         </div>
-                        <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                        <div class="col-sm-6 col-md-3 item text d-flex flex-column align-items-center">
+                            <h3>Tamaño de la empresa</h3>
+                            <p>1001-5000 empleados</p>
+                        </div>
+                        <div class="col-sm-6 col-md-3 item text d-flex flex-column align-items-center ">
+                            <h3>Fundación</h3>
+                            <p>1907</p>
+                        </div>
                     </div>
-                    <p class="copyright">Company Name © 2018</p>
+                    <p class="copyright">Grupo Uribe © 2023</p>
                 </div>
             </footer>
         </div>
