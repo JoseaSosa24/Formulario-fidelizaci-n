80a86e9cdbf1ffba378eba5216edd9f0cbd5000d
JOSE SOSA <josea_1998@hotmail.com>
2023-01-17 22:18:14 -0500

    creación del compotente header

--- front/src/components/helpers/Header/Header.jsx
+++ front/src/components/helpers/Header/Header.jsx
@@ -3,14 +3,7 @@ import "../Header/Header.css"
 export const Header = () => {
     return (
         <header>
-            <nav class="navbar bg-body-tertiary ">
-                <div class="container-fluid d-flex align-items-center">
-                    <a class="navbar-brand text-white d-flex align-items-center" href="#">
-                        <img src="../../../../src/assets/img/img-grupourube.jfif" alt="Logo" width="70" height="70" className="d-inline-block align-text-top" />
-                        <p>Grupo Uribe</p>
-                    </a>
-                </div>
-            </nav>
+                <img src='../../../../src/assets/img/banner-header.png'/>
         </header>
     )
 }
