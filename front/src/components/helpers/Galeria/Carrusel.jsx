86580019299a05306bd6ac7eb57c2e69c498795c
JOSE SOSA <josea_1998@hotmail.com>
2023-01-19 00:33:49 -0500

    Se cambian elementos del componente carusel

--- front/src/components/helpers/Galeria/Carrusel.jsx
+++ front/src/components/helpers/Galeria/Carrusel.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../Galeria/carrusel.css'
 import { CSSTransition } from 'react-transition-group';
-import { Titulo } from '../Titulo';
-import TypingEffect from '../TypingEffect';
+
 const images = [
     '../../../../src/assets/img/marcas/americaneagle.png',
     '../../../../src/assets/img/marcas/americanino.png',
@@ -25,35 +24,29 @@ const linksMarcas = [
 export const Galeria = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [currentIndexLinks, setCurrentIndexLinks] = useState(0);
+    const [contentVisible, setcontentVisible] = useState(false);
+
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrentIndex(currentIndex => (currentIndex + 1) % images.length);
             setCurrentIndexLinks(currentIndexLinks => (currentIndexLinks + 1) % linksMarcas.length)
         }, 5000);
+        setcontentVisible(true)
         return () => clearInterval(intervalId);
     }, []);
 
-    const text = [
-        "40% "
-    ];
-
     return (
         <>
-            <section className='titulos-informacion text-center mb-2'>
-                <h1 className='numero-descuento'>40%</h1>
-                <h6 className='descuento'>de descuento en TODAS</h6>
-                <h3 className=''>nuestras Marcas</h3>
-            </section>
-
-            <section className='galeria d-flex flex-column'>
+            <section className= "galeria d-flex flex-column ">
                 <CSSTransition in={true} timeout={2000} classNames="fade">
                     <a href={linksMarcas[currentIndexLinks]} target="_blank">
                         <img className='carousel-image' src={images[currentIndex]} id={currentIndex} alt="slide" width={270} />
                     </a>
                 </CSSTransition>
             </section>
-            <h6 className='mensaje'>click en la imagen para visitar la página oficial*</h6>
+            <h6 className='mensaje'>Click en la imagen para visitar la página oficial*</h6>
+
         </>
 
 
