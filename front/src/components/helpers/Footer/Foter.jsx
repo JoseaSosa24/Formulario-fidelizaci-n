9a7dfb19b80e530f1aca36ee6b8f4eff592f0c48
JOSE SOSA <josea_1998@hotmail.com>
2023-02-03 21:32:49 -0500

    Tratando de optimizar el footer

--- front/src/components/helpers/Footer/Foter.jsx
+++ front/src/components/helpers/Footer/Foter.jsx
@@ -1,57 +1,75 @@
-import React from 'react'
+import React from 'react';
 import '../Footer/footer.css'
-export const Footer = () => {
-    return (
-        <section className="footer-dark">
-            <footer>
-                <section className="container mt-4">
 
-                    <section className="row d-flex justify-content-center">
-                        <section className="col-md-4 item text">
-                            <h3>Grupo Uribe</h3>
-                            <p>Conjunto de empresas ubicada en Medellín del sector textil con representación,
-                                y/o producción y comercialización para Colombia de las marcas Chevignon,
-                                Naf Naf, Americanino, Esprit, Rifle, American Eagle, MNG.</p>
-                        </section>
-                        <section className="col-sm-6 col-md-3 item text">
-                            <h3>Tamaño de la empresa</h3>
-                            <p>1001-5000 empleados</p>
-                        </section>
-                        <section className="col-sm-6 col-md-2 item text ">
-                            <h3>Fundación</h3>
-                            <p>1907</p>
-                        </section>
-                        <section className="col-sm-6 col-md-3 item text  ">
-                            <section>
-                                <h3>Contacto</h3>
-                                <p>
-                                    MEDELLIN, ANTIOQUIA
-                                </p>
-                            </section>
-                            <section>
+const FooterInformation = ({ companyName, employeesNumber, foundationYear, companyDescription }) => (
+    <>
+        <section className="col-md-4 item text">
+            <h3>{companyName}</h3>
+            <p>{companyDescription}</p>
+        </section>
+        <section className="col-sm-6 col-md-3 item text">
+            <h3>Tamaño de la empresa</h3>
+            <p>{employeesNumber}</p>
+        </section>
+        <section className="col-sm-6 col-md-2 item text">
+            <h3>Fundación</h3>
+            <p>{foundationYear}</p>
+        </section>
+    </>
 
-                                <p>Carrera 52 #19-80 avenida guayabal</p>
-                            </section>
-                            <section>
-                                <p>050024 Medellin</p>
-                            </section>
-                            <section>
-                                <p>Colombia</p>
-                            </section>
-                            <section>
-                                <p>+57 (604) 6071213</p>
-                            </section>
-                            <section>
-                                <p>
-                                    <a href="#">mercadeo@johnuribe.com.co</a>
-                                </p>
-                            </section>
-                        </section>
+);
 
-                    </section>
-                    <p className="copyright">Grupo Uribe © 2023</p>
+const FooterContact = ({ location, address, city, country, phoneNumber, email }) => (
+    <section className="col-sm-6 col-md-3 item text">
+        <section>
+            <h3>Contacto</h3>
+            <p>{location}</p>
+        </section>
+        <section>
+            <p>{address}</p>
+        </section>
+        <section>
+            <p>{city}</p>
+        </section>
+        <section>
+            <p>{country}</p>
+        </section>
+        <section>
+            <p>{phoneNumber}</p>
+        </section>
+        <section>
+            <p>
+                <a href="#">{email}</a>
+            </p>
+        </section>
+    </section>
+);
+
+export const Footer = () => (
+    <section className="footer-dark">
+        <footer>
+            <section className="container mt-4">
+                <section className="row d-flex justify-content-center">
+                    <FooterInformation
+                        companyName="Grupo Uribe"
+                        employeesNumber="1001-5000 empleados"
+                        foundationYear="1907"
+                        companyDescription="Conjunto de empresas ubicada en Medellín del sector textil con representación, y/o producción y comercialización para Colombia de las marcas Chevignon, Naf Naf, Americanino, Esprit, Rifle, American Eagle, MNG."
+                    />
+                    <FooterContact
+                        location="MEDELLIN, ANTIOQUIA"
+                        address="Carrera 52 #19-80 avenida guayabal"
+                        city="050024 Medellin"
+                        country="Colombia"
+                        phoneNumber="+57 (604) 6071213"
+                        email="mercadeo@johnuribe.com.co"
+                    />
                 </section>
-            </footer >
-        </section >
-    )
-}
+                <p className="copyright">Grupo Uribe © 2023</p>
+                <p className="copyright">Hecho por <a href='https://www.linkedin.com/in/joseasosa24/'>José Sosa</a></p>
+            </section>
+        </footer>
+    </section>
+);
+
+export default Footer;
